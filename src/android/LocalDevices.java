b8a4a124fdd2625cf591840e6a5556beb1d51c0d
arys <arystankaliakparov@gmail.com>
2021-02-19 16:03:29 +0600

    fix: scan

--- src/android/LocalDevices.java
+++ src/android/LocalDevices.java
@@ -1,5 +1,7 @@
 package com.arystankaliakparov.cordova_plugin_local_devices;
 
+import android.util.Log;
+
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -7,55 +9,41 @@ import org.json.JSONException;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class LocalDevices extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
-
-        if (action.equals("greet")) {
-            String name = data.getString(0);
-            String message = "Hello, " + name;
-            callbackContext.success(message);
-            return true;
-        }
-
         if (action.equals("scan")) {
-            ArrayList<String> ipList = new ArrayList<>();
             String ip = Utils.getIPAddress(true);
             String[] ipSplit = ip.split("\\.");
             String subnet = ipSplit[0] + "." + ipSplit[1] + "." + ipSplit[2];
-
-            ExecutorService myExecutor = Executors.newCachedThreadPool();
-            myExecutor.execute(new Runnable() {
-                @Override
-                public void run() {
-                    JSONArray ips = scan(subnet);
-                    callbackContext.success(ips);
-                }
-            });
+            scan(subnet, callbackContext);
+            return true;
         }
 
         return false;
     }
 
-    JSONArray scan(String subnet) {
-        JSONArray ips = new JSONArray();
-        try {
-            for (int i = 1; i < 255; i++){
-                String ip = subnet + "." + i;
-                if (InetAddress.getByName(ip).isReachable(500)){
-                    ips.put(ip);
+    void scan(String subnet, CallbackContext callbackContext) {
+        cordova.getThreadPool().execute(new Runnable() {
+            @Override
+            public void run() {
+                JSONArray ips = new JSONArray();
+                try {
+                    for (int i = 1; i < 255; i++){
+                        String ip = subnet + "." + i;
+                        if (InetAddress.getByName(ip).isReachable(5)){
+                            ips.put(ip);
+                        }
+                    }
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
+                callbackContext.success(ips);
             }
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return ips;
+        });
     }
 }
--- www/local-devices.js
+++ www/local-devices.js
@@ -1,11 +1,7 @@
 /* global cordova */
 
 module.exports = {
-  greet(name, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, "LocalDevices", "greet", [name]);
-  },
   scan(successCallback, errorCallback) {
-    console.log("SCANNN");
     cordova.exec(successCallback, errorCallback, "LocalDevices", "scan", []);
   },
 };
